bcaf9a8930257a7e309d02edc57ed2bd40d9797d
John Griffith <john.griffith@live.co.uk>
2015-02-22 22:08:29 +0000

    Reading an integer from stdin

--- euler.c
+++ euler.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
+/*
+	Replicate built in function to reverse a string	
+*/
+char * strrev(char *str){
 
+	char *p1, *p2;
+	if(! str || ! *str){
+		return str;
+	}
+	for( p1 = str, p2 = str + strlen(str) -1 ; p2>p1;++p1,--p2){
+		*p1 ^= *p2;
+		*p2 ^= *p1;
+		*p1 ^= *p2;
+	}
+	return str;
+
+}
+
+/* 
+	Read the stdin for an integer, return an integer 
+	
+*/
 int readInt(){
 
 	int out = 0; 
@@ -14,21 +36,23 @@ int readInt(){
 	char * input = (char *)malloc(linelen*sizeof(char));
 	
 	char * ptr;
+	
+	
 	ptr = input;
 	fgets(input,linelen,stdin);
-	
+	input = strrev(input);
 	do{
 		if(*ptr != '\n' && (*ptr < '0' || *ptr > '9')){
 			printf("Invalid integer input");
-			return 0;
-			
-			
+			return 0;			
 		}
-	
-		out += *ptr - '0' * pow (10 , i);
-		i++;
+		if(*ptr <= '9' && *ptr >= '0'){
+			out += (*ptr - '0') * pow (10 , i);
+			i++;
+		}
 		ptr++;
 	}while(*ptr != '\0');
+
 	return out;
 }
 
@@ -38,7 +62,7 @@ int main(){
 	int in = readInt();
 
 		
-	printf("Input: %d\n", in);
+	
 
 	return 0;
 }
